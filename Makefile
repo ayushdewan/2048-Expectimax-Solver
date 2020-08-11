290d43a707cd5d948826e6a725f20de4a099f03b
ayushdewan <ayushdewan02@gmail.com>
2020-08-11 13:47:23 -0700

    singe trial execution

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS=-Wall -g
 
 all:
-	gcc -o output -O2 main.c matrix.c expectimax.c
+	gcc -o output -O2 main.c matrix.c expectimax.c -lpthread
 
 clean:
 	rm -f output
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+
 #include "matrix.h"
 #include "expectimax.h"
 
@@ -8,38 +10,32 @@ int main(int argc, char *argv[]) {
     time_t t;
     srand((unsigned) time(&t));
     
-    int event = 0;
-    for(int i = 0; i < 10; i++) {
-        matrix mat;
-        init_matrix(&mat);
-        spawn_tile(&mat);
+    matrix mat;
+    init_matrix(&mat);
+    spawn_tile(&mat);
+    spawn_tile(&mat);
+    //print_matrix(&mat);
+    
+    while(1) {
+        action a = expectimax(&mat, 0, 4);
+        if(a.move == 0) mat = push_north(&mat);
+        else if(a.move == 1) mat = push_south(&mat);
+        else if(a.move == 2) mat = push_east(&mat);
+        else if(a.move == 3) mat = push_west(&mat);
+        else { puts("done"); break; };
         spawn_tile(&mat);
         //print_matrix(&mat);
-        printf("%d\n", i + 1);
-        
-        while(1) {
-            action a = expectimax(&mat, 0, 4);
-            if(a.move == 0) mat = push_north(&mat);
-            else if(a.move == 1) mat = push_south(&mat);
-            else if(a.move == 2) mat = push_east(&mat);
-            else if(a.move == 3) mat = push_west(&mat);
-            else { puts("done"); break; };
-            spawn_tile(&mat);
-            //print_matrix(&mat);
-            //printf("");
-        }
-        
-        int solved = 0;
-        for(int j = 0; j < 4 && !solved; j++) {
-            for(int k = 0; k < 4 && !solved; k++) {
-                if(mat.grid[j][k] >= 2048) {
-                    solved = 1;
-                }
+        //puts("");
+    }
+    
+    int solved = 0;
+    for(int j = 0; j < 4 && !solved; j++) {
+        for(int k = 0; k < 4 && !solved; k++) {
+            if(mat.grid[j][k] >= 2048) {
+                solved = 1;
             }
         }
-        event += solved;
-        if(solved) puts("+");
     }
-    printf("%d\n", event);
+    if(solved) puts("+");
     return 0;
 }
